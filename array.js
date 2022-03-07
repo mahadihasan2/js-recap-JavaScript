0c2ac1a323bf081125b98be1369d28a4ebea90ab
mahadihasan2 <mahadihasan125643@gmail.com>
2022-03-07 22:26:38 +0600

    Array-Recap

---
+++ array.js
@@ -0,0 +1,7 @@
+const number = [30, 40, 55, 69, 80]
+// number.indexOf[0]
+console.log(number.indexOf(55))
+console.log(number.length)
+// number.push = 50;
+// console.log(number) 
+console.log(number.push = 50)
