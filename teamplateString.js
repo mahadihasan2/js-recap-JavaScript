db0ba6b9b82382309c71780faf40e92ce67b7a1c
mahadihasan2 <mahadihasan125643@gmail.com>
2022-03-07 22:43:38 +0600

    teamplateString Done

---
+++ teamplateString.js
@@ -0,0 +1,14 @@
+const office = {
+    name: 'BariDara 1 Number Road',
+    Employer: 50,
+    Work: {
+        Employer1: 'Web Developer',
+        Employer2: 'Digital Marketing',
+        Employer3: 'Seo Expert'
+
+    },
+    address: ['Bosundhora  #32 Road *buterGoli', 'Mirpur-13', 'Mirpur-14']
+}
+// Ussing TeamPlate String
+const about = `My office Name is: ${office.name}. there address is:${office.address[0]}`
+console.log(about)
