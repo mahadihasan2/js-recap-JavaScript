90cd7cd821a105ec24438c0e62907509bcda6c83
mahadihasan2 <mahadihasan125643@gmail.com>
2022-03-08 18:19:43 +0600

    ternary-Done

---
+++ ternary.js
@@ -0,0 +1,18 @@
+let money = 400;
+if (money > 300) {
+    console.log('birani')
+} else {
+    console.log('cha biskut')
+}
+// ternary 
+let food1 = money > 600 ? 'birani' : 'KurmaPula'
+console.log(food1);
+// ShortChut Number to String
+let num = 32;
+console.log(num);
+let num2 = num + '';
+console.log(num2)
+// String to Number 
+const input = '56'
+const input2 = +input;
+console.log(input2);
